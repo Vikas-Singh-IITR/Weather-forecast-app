dd1885c46993bbf8b38a5189b2a9e4ea3063f1e8
Vikas-Singh-IITR <vsingh@ar.iitr.ac.in>
2021-07-18 18:10:57 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const message_1 = document.getElementById("message-1");
+const message_2 = document.getElementById("message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  const url = `http://localhost:3000/weather?address=${location}`;
+
+  message_1.textContent = "Loading...";
+  message_2.textContent = "";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        message_1.textContent = data.error;
+        message_2.textContent = "";
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+        message_1.textContent = data.location;
+        message_2.textContent = data.forecast;
+      }
+    });
+});
---
+++ src/app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const weather = require("./utils/weather");
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../template/views");
+const partialsPath = path.join(__dirname, "../template/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Spatan",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About the Creator",
+    name: "Spartan",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Spartan",
+    helpText:
+      "Hello my name is spartan how can i help you to know your weather forecast",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  const address = req.query.address;
+  if (!address) {
+    return res.send({
+      error: "Please enter your location to continue",
+    });
+  }
+  geocode(address, (error, { longitude, latitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    weather(longitude, latitude, (error, forecast) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast,
+        location,
+      });
+    });
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404page", {
+    title: "404 Not Found",
+    name: "Spatan",
+    errorMessage: "The article you are looking for is not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404page", {
+    title: "404",
+    name: "Spatan",
+    errorMessage: "Something went wrong.",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
